780601dba19fb4f5fadce017e8dbe86bc5bce41b
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-10-14 21:52:05 +0200

    [f] added android project with .gitignore

---
+++ mobile-app/app/src/main/java/com/mojapl/mobile_app/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mojapl.mobile_app.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mojapl.mobile_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
