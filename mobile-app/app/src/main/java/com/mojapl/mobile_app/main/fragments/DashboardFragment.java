4132b85e2dcdc8d43e3fddba5dcd48f3e1d043b6
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-10-23 02:28:50 +0200

    Dashboard added + new icons, Settings activity added, main fragment replaced with pager adapter

--- mobile-app/app/src/main/java/com/mojapl/mobile_app/main/fragments/DashboardFragment.java
+++ mobile-app/app/src/main/java/com/mojapl/mobile_app/main/fragments/DashboardFragment.java
@@ -1,6 +1,5 @@
 package com.mojapl.mobile_app.main.fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,20 +9,14 @@ import android.view.ViewGroup;
 import com.mojapl.mobile_app.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Created by shkliare on 10/22/17.
  */
-public class MainFragment extends Fragment {
 
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
+public class DashboardFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
-
 }
---
+++ mobile-app/app/src/main/java/com/mojapl/mobile_app/main/fragments/DashboardListFragment.java
@@ -0,0 +1,22 @@
+package com.mojapl.mobile_app.main.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mojapl.mobile_app.R;
+
+/**
+ * Created by shkliare on 10/22/17.
+ */
+
+public class DashboardListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard_list, container, false);
+    }
+}
