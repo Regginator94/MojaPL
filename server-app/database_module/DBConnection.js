7607fd7a2923cc2f04f9ac2cb82fab1ea2437c4b
Jakub K <kabza94@gmail.com>
2017-11-15 10:08:43 +0100

    db handler added

--- server-app/database_module/DBConnection.js
+++ server-app/database_module/DBConnection.js
@@ -15,12 +15,34 @@ var secretKey = 'adssad1';
 //   database : 'mojapl_db',
 // });
 
-var connection = mysql.createConnection({
+var connection; 
+
+function handleDisconnect() {
+  connection = = mysql.createConnection({
   host     : 'us-cdbr-iron-east-05.cleardb.net',
   user     : 'bf038d7d725ad7',
   password : '0dfef14d',
   database : 'heroku_cd344eaef5f5056',
-});
+});  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 setInterval(function () {
     connection.query('SELECT 1');
