712ce88ead0d447911529a4e1b417b0f75c7822d
Jakub K <kabza94@gmail.com>
2017-11-20 18:19:22 +0100

    category filter bug resolved

--- server-app/database_module/NewsGetters.js
+++ server-app/database_module/NewsGetters.js
@@ -2,11 +2,9 @@ var EventDBItem = require('./../objects/EventDBItem');
 
 exports.getNewsByOrgs = function(connection, response, userId){
 	var eventDBList = [];
-	var query = "SELECT E_ID, E_O_ID, E_C_ID, E_TEXT, E_TITLE, E_IMAGE_URL, E_START_DATE, E_CREATE_DATE, E_URL, E_FB_POST,"+
-		"organizations.O_NAME FROM events JOIN organizations ON events.E_O_ID = organizations.O_ID "+
-		"JOIN categories ON events.E_C_ID = categories.C_ID where O_ID IN (SELECT UFBO_ORGS FROM users_filters_by_org WHERE UFBO_U_ID="+userId+")";
+	var ufboQuery = "SELECT UFBO_ORGS FROM users_filters_by_org WHERE UFBO_U_ID="+userId;
 
-	connection.query(query, function (err, result, fields) {
+	connection.query(ufboQuery, function (err, result, fields) {
 	    if (err){
 	    	response.status(503);
 	    	response.json({
@@ -17,35 +15,46 @@ exports.getNewsByOrgs = function(connection, response, userId){
 	    	throw err;
 	    } 
 	    else{
-	    	if(result.length > 0){
-	    		for(var i = 0; i < result.length; i++){
-		    		var row = result[i];
-		    		var eventDB = new EventDBItem(row.E_ID, row.E_O_ID, row.E_C_ID, 
-		    			row.E_TEXT, row.E_TITLE, row.E_IMAGE_URL, row.E_START_DATE, row.E_CREATE_DATE, row.E_URL, row.E_FB_POST, row.O_NAME);
-		    		eventDBList.push(eventDB);
-		    	}
-		    	response.status(200);
-		    	response.json(eventDBList)	
-	    	} else {
-	    		response.status(503);
-		    	response.json({
-	                    status:503,
-	                    message:'No data for those filters.'
-	                });
-	    	}
+	    	const query = "SELECT E_ID, E_O_ID, E_C_ID, E_TEXT, E_TITLE, E_IMAGE_URL, E_START_DATE, E_CREATE_DATE, E_URL, E_FB_POST,"+
+			"organizations.O_NAME FROM events JOIN organizations ON events.E_O_ID = organizations.O_ID "+
+			"JOIN categories ON events.E_C_ID = categories.C_ID where O_ID IN ("+result[0].UFBO_ORGS+")";
+			console.log(query);
+	    	connection.query(query, function(err, result, fields){
+	    		if (err){
+			    	response.status(503);
+			    	response.json({
+		                    status:503,
+		                    message:'Data base error'
+		                });
+			    	console.log(err);
+			    	throw err;
+			    } else {
+	    		    	if(result.length > 0){
+				    		for(var i = 0; i < result.length; i++){
+					    		var row = result[i];
+					    		var eventDB = new EventDBItem(row.E_ID, row.E_O_ID, row.E_C_ID, 
+					    			row.E_TEXT, row.E_TITLE, row.E_IMAGE_URL, row.E_START_DATE, row.E_CREATE_DATE, row.E_URL, row.E_FB_POST, row.O_NAME);
+					    		eventDBList.push(eventDB);
+					    	}
+					    	response.status(200);
+					    	response.json(eventDBList)	
+				    	} else {
+				    		response.status(503);
+					    	response.json({
+				                    status:503,
+				                    message:'No data for those filters.'
+				                });
+				    	}
+			    }
+	    	})
 	    }
 	  });
 }
 
 exports.getNewsFiltered = function(connection, response,  categoryId, userId){	
 	var eventDBList = [];
-	var query = "SELECT E_ID, E_O_ID, E_C_ID, E_TEXT, E_TITLE, E_IMAGE_URL, E_CREATE_DATE, E_URL, E_FB_POST,"+
-		"organizations.O_NAME FROM events join organizations on events.E_O_ID = organizations.O_ID "+
-		"join categories on events.E_C_ID = categories.C_ID where E_C_ID = "+categoryId+" and "+
-		"O_ID IN (SELECT UFBO_ORGS FROM users_filters_by_org WHERE UFBO_U_ID="+userId+")";
-
-	connection.query(query, function (err, result, fields) {
-		console.log(result);
+	var ufboQuery = "SELECT UFBO_ORGS FROM users_filters_by_org WHERE UFBO_U_ID="+userId;
+	connection.query(ufboQuery, function (err, result, fields) {
 	    if (err){
 	    	response.status(503);
 	    	response.json({
@@ -56,22 +65,38 @@ exports.getNewsFiltered = function(connection, response,  categoryId, userId){
 	    	throw err;
 	    } 
 	    else{
-	    	if(result.length > 0){
-	    		for(var i = 0; i < result.length; i++){
-		    		var row = result[i];
-		    		var eventDB = new EventDBItem(row.E_ID, row.E_O_ID, row.E_C_ID, 
-		    			row.E_TEXT, row.E_TITLE, row.E_IMAGE_URL, row.E_START_DATE, row.E_CREATE_DATE, row.E_URL, row.E_FB_POST, row.O_NAME);
-		    		eventDBList.push(eventDB);
-		    	}
-		    	response.status(200);
-		    	response.json(eventDBList)	
-	    	} else {
-	    		response.status(503);
-		    	response.json({
-	                    status:503,
-	                    message:'No data for those filters.'
-	                });
-	    	}
+	    	const query = "SELECT E_ID, E_O_ID, E_C_ID, E_TEXT, E_TITLE, E_IMAGE_URL, E_START_DATE, E_CREATE_DATE, E_URL, E_FB_POST,"+
+			"organizations.O_NAME FROM events JOIN organizations ON events.E_O_ID = organizations.O_ID "+
+			"JOIN categories ON events.E_C_ID = categories.C_ID where E_C_ID = "+categoryId+" and "+
+			"E_O_ID IN ("+result[0].UFBO_ORGS+")";
+	    	connection.query(query, function(err, result, fields){
+	    		if (err){
+			    	response.status(503);
+			    	response.json({
+		                    status:503,
+		                    message:'Data base error'
+		                });
+			    	console.log(err);
+			    	throw err;
+			    } else {
+    		    	if(result.length > 0){
+			    		for(var i = 0; i < result.length; i++){
+				    		var row = result[i];
+				    		var eventDB = new EventDBItem(row.E_ID, row.E_O_ID, row.E_C_ID, 
+				    			row.E_TEXT, row.E_TITLE, row.E_IMAGE_URL, row.E_START_DATE, row.E_CREATE_DATE, row.E_URL, row.E_FB_POST, row.O_NAME);
+				    		eventDBList.push(eventDB);
+				    	}
+				    	response.status(200);
+				    	response.json(eventDBList)	
+			    	} else {
+			    		response.status(503);
+				    	response.json({
+			                    status:503,
+			                    message:'No data for those filters.'
+			                });
+			    	}
+			    }
+	    	})
 	    }
 	  });
 }
