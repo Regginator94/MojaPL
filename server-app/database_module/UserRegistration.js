579c00ba9ff3cdd9a1b8509c50eff63421e6ed1c
Jakub K <kabza94@gmail.com>
2017-12-02 13:39:39 +0100

    pass, email policy

--- server-app/database_module/UserRegistration.js
+++ server-app/database_module/UserRegistration.js
@@ -1,44 +1,80 @@
-const uuidV1 = require('uuid/v1');
 
 const POLITECHNIKA_ORG_ID = 1;
 exports.addUser = function(connection, response, request){
 	var email = request.body.email;
 	var password = request.body.password;
 	var facultyId = request.body.facultyId;
-	var userToken = uuidV1();
 	var query = "SELECT EXISTS(SELECT * FROM users WHERE U_EMAIL LIKE '"+email+"')";
 
-	connection.query(query, function (err, result, fields) {
-	    if (err){
-	    	throw err;
-	    	incorrectRegistrationResponse(response, email)
-	    } 
-	    else{
-	    	if(!userExists(result)) {
-	    		query = 'INSERT INTO users (U_EMAIL, U_PASSWORD, U_CREATE_DATE)  VALUES ("'+email+'", "'+password+'", NOW())';
-	    		connection.query(query, function (err, result, fields) {
-		    			if (err){
-					    	throw err;
-					    	incorrectRegistrationResponse(response, email)
-					    } 
-					    else {
-					    	query = 'INSERT INTO users_filters_by_org (UFBO_U_ID, UFBO_ORGS) VALUES ('+result.insertId+',"'+
-					    	POLITECHNIKA_ORG_ID+','+facultyId+'")';
-					    	connection.query(query, function (err, result, fields) {
-			    			if (err){
-						    	throw err;
-						    	incorrectRegistrationResponse(response, email);
-						    } else {
-		    		    		userCreatedResponse(response)
-						    }
-					    });
-		    		}
-		    	});
-	    	} else {
-	    		userAlreadyExists(response, email);
-	    	}
-	    }
-   	});
+	if(!validateEmail(email)){
+		incorrectRegistrationEmail(response);
+	}
+	else if(!validatePassword(password)){
+		incorrectRegistrationPassword(response);
+	}
+	else {
+		connection.query(query, function (err, result, fields) {
+			if (err){
+				throw err;
+				incorrectRegistrationResponse(response, email)
+			} 
+			else{
+				if(!userExists(result)) {
+					query = 'INSERT INTO users (U_EMAIL, U_PASSWORD, U_CREATE_DATE)  VALUES ("'+email+'", "'+password+'", NOW())';
+					connection.query(query, function (err, result, fields) {
+							if (err){
+								throw err;
+								incorrectRegistrationResponse(response, email)
+							} 
+							else {
+								query = 'INSERT INTO users_filters_by_org (UFBO_U_ID, UFBO_ORGS) VALUES ('+result.insertId+',"'+
+								POLITECHNIKA_ORG_ID+','+facultyId+'")';
+								connection.query(query, function (err, result, fields) {
+								if (err){
+									throw err;
+									incorrectRegistrationResponse(response, email);
+								} else {
+									userCreatedResponse(response)
+								}
+							});
+						}
+					});
+				} else {
+					userAlreadyExists(response, email);
+				}
+			}
+		   });
+	}
+}
+
+function validatePassword(password)
+{
+  // at least one number, one lowercase and one uppercase letter
+  // at least six characters
+  var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  return re.test(password);
+}
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function incorrectRegistrationEmail(response){
+	response.status(500);
+    response.json({
+        status:false,
+        message:'Incorrect user email.'
+    })
+}
+
+function incorrectRegistrationPassword(response){
+	response.status(500);
+    response.json({
+        status:false,
+        message:'Incorrect user password.'
+    })
 }
 
 function userExists(result){
